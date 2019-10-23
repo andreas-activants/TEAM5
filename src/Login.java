629d4cebf7446ebb4e92e54a837cade02519017a
PRiSMS DEV <prisms.dev@gmail.com>
2019-10-23 14:56:42 +0800

    Completed user login function

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,19 @@
 
 public class Login {
-
-	public Login() {
+	String username;
+	String password;
+	
+	public Login(String username, String password) {
 		//this is constructor
+		this.username = username;
+		this.password = password;
 	}
+	
+	public boolean validateLogin() {
+		if (this.username == "" || this.password == "") {
+			return false;
+		}
+		return true;
+	}
+	
 }
